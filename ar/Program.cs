0acf5501787f716df8e4c1e33b2dc4a0cb397f36
Krilin97 <miguelangel.espinosa.luna@ces-vegamedia.es>
2024-03-10 18:54:27 +0100

    Agregar archivos de proyecto.

---
+++ ar/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            String cadena;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Dime numero["+i+"]: ");
+                cadena = Console.ReadLine();
+                array[i] = int.Parse(cadena);
+            }
+
+            for (int i = 0;i < array.Length;i++)
+            {
+                Console.Write(array[i]);
+            }
+        }
+    }
+}
